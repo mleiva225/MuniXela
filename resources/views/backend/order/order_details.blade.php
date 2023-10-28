b4ed1ca2447545906db2c489071b0b186d37fb4a
Marvin Leiva <marleiq@hotmail.com>
2023-10-27 22:48:03 -0600

    Firmas

--- resources/views/backend/order/order_details.blade.php
+++ resources/views/backend/order/order_details.blade.php
@@ -1,6 +1,13 @@
 @extends('admin_dashboard')
 @section('admin')
 
+<style>
+    .firma-column {
+        display: inline-block;
+        width: 49%; /* Puedes ajustar el ancho según tus necesidades */
+        margin-top: 50px;
+    }
+</style>
 
 <div class="content">
 
@@ -48,7 +55,7 @@
                         <div class="row">
                             <div class="col-md-6">
                                 <div class="mt-3">
-                                    <p><b> NOMBRE DE EMPLEADO RESPONSABLE: <span >{{ $order->Users->name }} {{ $order->Users->lastname }}</span></b></p>
+                                    <p><b> NOMBRE DE EMPLEADO RESPONSABLE: <span>{{ $order->Users->name }} {{ $order->Users->lastname }}</span></b></p>
                                     <p><b> DEPENDENCIA: <span style="width: 150px;">{{ $order->Users->dependence }}</span></b></p>
                                     <p><b> CARGO: {{ $order->Users->position }}</b></p>
                                     <p><strong>FECHA : </strong> <span>{{ $order->created_at }}</span></p>
@@ -58,7 +65,7 @@
                             </div><!-- end col -->
                             <div class="col-md-4 offset-md-2">
                                 <div class="mt-3 float-end">
-                                    <p><strong>Serie :  </strong> <span class="float-end">{{ $order->series }}</span></p>
+                                    <p><strong>Serie : </strong> <span class="float-end">{{ $order->series }}</span></p>
                                 </div>
                             </div><!-- end col -->
                         </div>
@@ -111,37 +118,50 @@
                                                                 <b>{{ $item->Items->name }}</b> <br />
                                                             </td>
                                                             <td>
-                                                            {{ $item->Items->name }}
+                                                                {{ $item->Items->name }}
                                                             </td>
-                                                            <td class="text-end">Q. {{ $item->Items->unit_value }}
+                                                            <td class="text-end">Q. {{ number_format($item->Items->unit_value, 2) }}
 
                                                             </td>
                                                         </tr>
                                                         @php
-                                                            $totalPrice += $item->Items->unit_value;                                                         @endphp
+                                                        $totalPrice += $item->Items->unit_value; @endphp
                                                         @endforeach
                                                         <tr>
                                                             <td colspan="5" align="center">Final de línea</td>
                                                         </tr>
                                                         <tr>
                                                             <td colspan="4" align="right"><b>Total:</b></td>
-                                                            <td class="text-end">
-                                                                <b>Q. {{ $totalPrice }}</b>
+                                                            <td>
+                                                                <b>Q. {{ number_format($totalPrice, 2) }}</b>
                                                             </td>
                                                         </tr>
-
                                                     </tbody>
                                                 </table>
+                                                <div></div>
+
+                                                <div class="firma-column">
+                                                    <div class="text-center">
+                                                        Firma del Responsable <br>
+                                                        {{ $order->Users->name }} {{ $order->Users->lastname }}
+                                                    </div>
+                                                </div>
+                                                <div class="firma-column">
+                                                    <div class="text-center">
+                                                        {{ auth()->user()->name }} {{ auth()->user()->lastname }}<br>
+                                                        {{ auth()->user()->position }} {{ auth()->user()->dependence }}
+                                                    </div>
+                                                </div>
                                             </div> <!-- end table-responsive -->
                                         </div> <!-- end col -->
 
                                         <div class="mt-4 mb-1">
-                            <div class="text-end d-print-none">
-                                <a href="javascript:window.print();" class="btn btn-success rounded-pill waves efect waves-light">
-                                    <i class="mdi mdi-printer me-1"></i> Imprimir
-                                </a>
-                            </div>
-                        </div>
+                                            <div class="text-end d-print-none">
+                                                <a href="javascript:window.print();" class="btn btn-success rounded-pill waves efect waves-light">
+                                                    <i class="mdi mdi-printer me-1"></i> Imprimir
+                                                </a>
+                                            </div>
+                                        </div>
 
                                     </div>
                                 </div>
